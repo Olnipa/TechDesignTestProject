86b8413d24ad1cd49346269456ee0b710956f84d
Pakhimskii ON <oleghack@gmail.com>
2023-11-30 12:23:46 +0700

    Created Scene Switcher
    1. Implemented logic of switching the scenes

---
+++ Assets/Scripts/BootStrap.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class BootStrap : MonoBehaviour
+{
+    [SerializeField] private SceneSwitchDetector _sceneSwitchDetector;
+
+    [SerializeField] private SceneSwitcher _sceneSwitcher;
+    //private CompositeDisposable _compositeDisposable;
+
+    private void Start()
+    {
+        //_compositeDisposable = new CompositeDisposable();
+
+        _sceneSwitcher.Initialize(_sceneSwitchDetector);
+    }
+
+    //private void OnDestroy()
+    //{
+    //    _compositeDisposable.DisposeAll();
+    //}
+}
+
+//public class CompositeDisposable
+//{
+//    private List<IDisposable> _disposables = new List<IDisposable>();
+
+//    public void Add(IDisposable disposable)
+//    {
+//        _disposables.Add(disposable);
+//    }
+
+//    public void DisposeAll()
+//    {
+//        foreach(IDisposable disposable in _disposables)
+//        {
+//            disposable.Dispose();
+//        }
+//    }
+//}
---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SceneSwitcher : MonoBehaviour
+{
+    private SceneSwitchDetector _sceneSwitchDetector;
+    private Button _nextSceneButton;
+    private string _nextSceneName;
+
+    private void OnDisable()
+    {
+        _nextSceneButton.onClick.RemoveListener(OpenNextScene);
+        _nextSceneButton = null;
+    }
+
+    private void OnDestroy()
+    {
+        _sceneSwitchDetector.SceneSwitcherClicked -= Activate;
+    }
+
+    public void Initialize(SceneSwitchDetector sceneSwitchDetector)
+    {
+        _sceneSwitchDetector = sceneSwitchDetector;
+        _nextSceneButton = GetComponent<Button>();
+
+        _sceneSwitchDetector.SceneSwitcherClicked += Activate;
+    }
+
+    public void Activate(string nextSceneName)
+    {
+        gameObject.SetActive(true);
+        _nextSceneName = nextSceneName;
+        _nextSceneButton.onClick.AddListener(OpenNextScene);
+    }
+
+    private void OpenNextScene()
+    {
+        if (_nextSceneName == null)
+            return;
+
+        SceneManager.LoadScene(_nextSceneName);
+    }
+}
