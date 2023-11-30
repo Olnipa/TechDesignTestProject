70c49d8f0cb0bf32d3e5994965cae3ee103ce77c
Pakhimskii ON <oleghack@gmail.com>
2023-12-01 01:24:59 +0700

    Project cleaning
    1. Object renaming
    2. Removed some unused assets

--- Assets/Scripts/BootStrap.cs
+++ Assets/Scripts/BootStrap.cs
@@ -3,37 +3,11 @@
 public class BootStrap : MonoBehaviour
 {
     [SerializeField] private SceneSwitcherRevealler _sceneSwitchDetector;
-
     [SerializeField] private SceneSwitcher _sceneSwitcher;
-    //private CompositeDisposable _compositeDisposable;
 
     private void Start()
     {
-        //_compositeDisposable = new CompositeDisposable();
-
         _sceneSwitcher.Initialize(_sceneSwitchDetector);
     }
 
-    //private void OnDestroy()
-    //{
-    //    _compositeDisposable.DisposeAll();
-    //}
 }
-
-//public class CompositeDisposable
-//{
-//    private List<IDisposable> _disposables = new List<IDisposable>();
-
-//    public void Add(IDisposable disposable)
-//    {
-//        _disposables.Add(disposable);
-//    }
-
-//    public void DisposeAll()
-//    {
-//        foreach(IDisposable disposable in _disposables)
-//        {
-//            disposable.Dispose();
-//        }
-//    }
-//}
